f905f71b109a21891f7eca529485ae094ba2c4fd
unknownsea <seasoank@gmail.com>
2025-03-03 01:27:26 -0700

    init (this will get better)

---
+++ src/main/java/com/example/addon/modules/AutoWB.java
@@ -0,0 +1,22 @@
+package com.example.addon.modules;
+
+import com.example.addon.AddonTemplate;
+import meteordevelopment.meteorclient.events.game.ReceiveMessageEvent;
+import meteordevelopment.meteorclient.systems.modules.Module;
+import meteordevelopment.orbit.EventHandler;
+import net.minecraft.client.MinecraftClient;
+
+public class AutoWB extends Module {
+
+    public AutoWB() {
+        super(AddonTemplate.CATEGORY, "auto-wb", "Responds with '/wb' when a chat message contains '/wb'.");
+    }
+
+    @EventHandler
+    private void onChatReceived(ReceiveMessageEvent event) {
+        String message = event.getMessage().getString();
+        if (message.contains("/wb")) {
+            MinecraftClient.getInstance().player.networkHandler.sendChatMessage("/wb");
+        }
+    }
+}
